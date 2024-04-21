312417f548ee862d55672424f572b086ed89e3f1
radovanbotik <radovanbotik@gmail.com>
2024-04-21 16:57:35 +0200

    footer naming

--- app/ui/Layout.tsx
+++ app/ui/Layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 
-import Footer from "./Footer";
+import Footer from "./Foot";
 import Header from "./Header";
 
 export default function Layout({ children }: { children: ReactNode }) {
